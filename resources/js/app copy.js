5615fb8ede77537377ebfb183bd6c4fb3827c06d
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-11-17 17:02:39 -0600

    Created style sheet for enrollment html page

--- resources/js/app copy.js
+++ resources/js/app copy.js
@@ -5,27 +5,27 @@ function buildCharts(sample) {
 
     var data = enrollData;
     // console.log(data);
-
-    var kept = data.filter(coverage => coverage['TYPE OF ENROLLMENT CHANGE'] == 'Kept Coverage'); // Used for Bar Charts
-    var added = data.filter(coverage => coverage['TYPE OF ENROLLMENT CHANGE'] == 'Added Coverage'); // Used for Bar Charts
-    // console.log(kept);
-    // console.log(added);
-    
     var year = data.map(year => year['year']);
     var enrollmentChange = data.map(enrollment => enrollment['TYPE OF ENROLLMENT CHANGE']);
     var planType = data.map(type => type['PLAN TYPE']);
     var planName = data.map(pname => pname['PLAN NAME']);
     var name = data.map(ename => ename['NAME']);
-    // console.log(year);
-    // console.log(enrollmentChange);
-    // console.log(planType);
-    // console.log(planName);
-    // console.log(name);
+    console.log(year);
+    console.log(enrollmentChange);
+    console.log(planType);
+    console.log(planName);
+    console.log(name);
+
     buildTable(year, enrollmentChange, planType, planName, name);
+
+    var kept = data.filter(coverage => coverage['TYPE OF ENROLLMENT CHANGE'] == 'Kept Coverage');
+    var added = data.filter(coverage => coverage['TYPE OF ENROLLMENT CHANGE'] == 'Added Coverage');
+    console.log(kept);
+    console.log(added);
+
 
     // (https://stackoverflow.com/questions/29364262/how-to-group-by-and-sum-array-of-object)
     var sumKept = [];
-    // (https://www.w3schools.com/jsref/jsref_reduce.asp)
     kept.reduce(function (kpt, value) {
       if (!kpt[value['PLAN TYPE']]) {
         kpt[value['PLAN TYPE']] = { 'PLAN TYPE': value['PLAN TYPE'], '# of Employees': 0 };
@@ -48,6 +48,7 @@ function buildCharts(sample) {
     console.log(sumKept);
     console.log(sumAdded);
 
+
     var sortedKept = sumKept.sort(function (a, b) {
       return b['# of Employees'] - a['# of Employees']
     });
@@ -60,8 +61,8 @@ function buildCharts(sample) {
       x: sumKept.map(row => row['PLAN TYPE']),
       y: sumKept.map(row => row['# of Employees']),
       text: "Kept Coverage",
-      // text: sumkept.map(row => row['# of Employees']),
-      // textposition: 'auto',
+      text: sumKept.map(row => row['# of Employees']),
+      textposition: 'auto',
       name: 'Kept Coverage',
     };
 
@@ -69,8 +70,8 @@ function buildCharts(sample) {
       type: 'bar',
       x: sumAdded.map(row => row['PLAN TYPE']),
       y: sumAdded.map(row => row['# of Employees']),
-      // text: sumAdded.map(row => row['# of Employees']),
-      // textposition: 'auto',
+      text: sumAdded.map(row => row['# of Employees']),
+      textposition: 'auto',
       name: 'Added Coverage',
     };
 
@@ -87,12 +88,11 @@ function buildCharts(sample) {
 
     Plotly.newPlot('enrollment', data, layout, config);
 
-    
     function buildTable(year, enrollmentChange, planType, planName, name) {
       var table = d3.select("#summary-table");
       var tbody = table.select("tbody");
       var trow;
-      for (var i = 0; i < 100; i++) {
+      for (var i = 0; i < 12; i++) {
         trow = tbody.append("tr");
         trow.append("td").text(year[i]);
         trow.append("td").text(enrollmentChange[i]);
